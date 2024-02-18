064051b40e729ed3698630a10af85617604a0b8e
rshashank11 <shashankyadav2000@gmail.com>
2024-02-18 20:11:02 +0530

    Commit

--- src/api/baseAPI.js
+++ src/api/baseAPI.js
@@ -1,3 +1,2 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
-export const baseURL = `https://pixabay.com/api` + `?key=${API_KEY}`;
-console.log(baseURL);
+export const baseURL = `https://pixabay.com/api?key=${API_KEY}`;
